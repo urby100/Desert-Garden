cd922dcb1cf50a2f7dac4da94a49be7bdbbe2317
urby100 <urbanbozic1996@gmail.com>
2019-01-30 13:15:48 +0100

    Save

---
+++ Assets/Animations.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    ScriptableObject so;
+    float moving;
+
+    private bool hurting = false;
+    private float hurtLenght = 0.3f;
+    private float hurtStart;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //obračanje
+        moving = Input.GetAxisRaw("Horizontal");
+        if (gameObject.GetComponent<Move>().hurtRequest)
+        {
+            moving = 0;
+        }
+        else if (gameObject.GetComponent<Move>().tiredRequest) {
+
+            moving = 0;
+        }
+        if (moving == -1)
+        {
+            sr.flipX = true;
+        }
+        else if (moving == 1)
+        {
+            sr.flipX = false;
+
+        }
+
+        //hurt
+        if (hurting && Time.time >= hurtStart)
+        {
+            gameObject.GetComponent<Move>().hurtRequest = false;
+            hurting = false;
+        }
+        if (gameObject.GetComponent<Move>().hurtRequest)
+        {
+            hurt();
+            if (!hurting)
+            {
+                hurtStart = Time.time + hurtLenght;
+                hurting = true;
+            }
+            return;
+        }
+
+        if (rb.velocity.y < 0)// falling
+        {
+            falling();
+        }
+        else if (rb.velocity.y > 0)//jump
+        {
+            if (!Input.GetKey(KeyCode.UpArrow))
+            {
+                jumping();
+            }
+            else
+            {
+                movingMidAir();
+            }
+        }
+        else//landed
+        {
+            if (gameObject.GetComponent<Move>().tiredRequest)
+            {
+                tired();
+                return;
+            }
+            if (rb.velocity.x != 0)
+            {
+                running();
+            }
+            else
+            {
+                if (gameObject.GetComponent<Move>().crouchRequest)
+                {
+                    crouching();
+                }
+                else
+                {
+                    standing();
+                }
+            }
+        }
+
+    }
+
+    void standing()
+    {
+        animator.SetBool("Standing", true);
+        animator.SetBool("Crouching", false);
+        animator.SetBool("Running", false);
+        animator.SetBool("Jumping", false);
+        animator.SetBool("Falling", false);
+        animator.SetBool("MovingMidAir", false);
+        animator.SetBool("Hurt", false);
+        animator.SetBool("Tired", false);
+
+    }
+    void crouching()
+    {
+        animator.SetBool("Standing", false);
+        animator.SetBool("Crouching", true);
+        animator.SetBool("Running", false);
+        animator.SetBool("Jumping", false);
+        animator.SetBool("Falling", false);
+        animator.SetBool("MovingMidAir", false);
+        animator.SetBool("Hurt", false);
+        animator.SetBool("Tired", false);
+    }
+    void running()
+    {
+        animator.SetBool("Standing", false);
+        animator.SetBool("Crouching", false);
+        animator.SetBool("Running", true);
+        animator.SetBool("Jumping", false);
+        animator.SetBool("Falling", false);
+        animator.SetBool("MovingMidAir", false);
+        animator.SetBool("Hurt", false);
+        animator.SetBool("Tired", false);
+
+    }
+    void jumping()
+    {
+        animator.SetBool("Standing", false);
+        animator.SetBool("Crouching", false);
+        animator.SetBool("Running", false);
+        animator.SetBool("Jumping", true);
+        animator.SetBool("Falling", false);
+        animator.SetBool("MovingMidAir", false);
+        animator.SetBool("Hurt", false);
+        animator.SetBool("Tired", false);
+
+    }
+    void falling()
+    {
+        animator.SetBool("Standing", false);
+        animator.SetBool("Crouching", false);
+        animator.SetBool("Running", false);
+        animator.SetBool("Jumping", false);
+        animator.SetBool("Falling", true);
+        animator.SetBool("MovingMidAir", false);
+        animator.SetBool("Hurt", false);
+        animator.SetBool("Tired", false);
+
+    }
+    void movingMidAir()
+    {
+        animator.SetBool("Standing", false);
+        animator.SetBool("Crouching", false);
+        animator.SetBool("Running", false);
+        animator.SetBool("Jumping", false);
+        animator.SetBool("Falling", false);
+        animator.SetBool("MovingMidAir", true);
+        animator.SetBool("Hurt", false);
+        animator.SetBool("Tired", false);
+
+    }
+    void hurt()
+    {
+        animator.SetBool("Standing", false);
+        animator.SetBool("Crouching", false);
+        animator.SetBool("Running", false);
+        animator.SetBool("Jumping", false);
+        animator.SetBool("Falling", false);
+        animator.SetBool("MovingMidAir", false);
+        animator.SetBool("Hurt", true);
+        animator.SetBool("Tired", false);
+    }
+    void tired()
+    {
+        animator.SetBool("Standing", false);
+        animator.SetBool("Crouching", false);
+        animator.SetBool("Running", false);
+        animator.SetBool("Jumping", false);
+        animator.SetBool("Falling", false);
+        animator.SetBool("MovingMidAir", false);
+        animator.SetBool("Hurt", false);
+        animator.SetBool("Tired", true);
+
+    }
+    void animationControl(string animationName)
+    {
+
+
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float moveInput;
+
+    float movementSpeed = 6f;
+
+    bool jumpRequest;
+    public bool crouchRequest;
+    public bool tiredRequest;
+    public bool hurtRequest;
+    bool moveAfterHurt;
+
+
+    float minXPosition = -2.5f;
+    float maxXPosition = 1000f;
+    float xPosition;
+
+
+    float jumpVelocity = 9f;
+    float jumpCounter = 0f;
+    float fallMultiplier = 5f;
+    float lowJumpMultiplier = 5f;
+    int maxJumps = 1;
+    
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiredRequest = false;
+        moveAfterHurt = false;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            hurtRequest= true;
+            moveAfterHurt = true;
+        }
+        if (Input.GetKey(KeyCode.Y))
+        {
+            tiredRequest = true;
+        }else if (Input.GetKey(KeyCode.X))
+        {
+            tiredRequest = false;
+        }
+        if (tiredRequest)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jumpCounter++;
+            jumpRequest = true;
+        }
+        if (Input.GetKey(KeyCode.C) || Input.GetKey(KeyCode.DownArrow)) {
+            crouchRequest = true;
+        } else if (Input.GetKeyUp(KeyCode.C) || Input.GetKeyUp(KeyCode.DownArrow)) {
+            crouchRequest = false;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //better jump
+        if (rb.velocity.y < 0 || hurtRequest)
+        {
+            rb.gravityScale = fallMultiplier;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.gravityScale = lowJumpMultiplier;
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+
+        //move
+        moveInput = Input.GetAxisRaw("Horizontal");
+        if (crouchRequest && rb.velocity.y == 0)//če je na tleh in se hoče skloniti se ne sme premikati...
+        {
+            moveInput = 0;
+        }
+        if (tiredRequest || hurtRequest )//če je umrl ali bil zadet
+        {
+            moveInput = 0;
+        }
+        if (moveAfterHurt) {//počakam da je animacija ko je bil zadet končana
+            moveInput = 0;
+        }
+        if ((transform.position.x <=minXPosition && moveInput==-1) 
+            || (transform.position.x >= maxXPosition && moveInput==1))
+        {//limitiram premik levo in desno
+            moveInput = 0;
+        }
+        rb.velocity = new Vector2(moveInput * movementSpeed, rb.velocity.y);
+        //jump
+
+        if (tiredRequest || hurtRequest)
+        {
+
+            return;
+        }
+        if (jumpRequest && jumpCounter <= maxJumps)
+        {
+            rb.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
+            jumpRequest = false;
+        }
+        if (rb.velocity.y == 0)
+        {
+            moveAfterHurt = false;
+            jumpCounter = 0;
+            jumpRequest = false;
+        }
+    }
+}
