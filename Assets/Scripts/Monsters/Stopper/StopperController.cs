15a0e006bf6649c32b9b6101d9824d86bfbc811b
urby100 <urbanbozic1996@gmail.com>
2019-02-05 17:39:56 +0100

    Added sprites and animations for 2 monsters

---
+++ Assets/Scripts/Monsters/Stopper/StopperController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopperController : MonoBehaviour
+{
+    float globalDelay = 0f;//za drugačni delay pri streljanju ko je več enakih 
+    public GameObject stopperBody;
+    public GameObject stopperProjectile;
+    public GameObject projectileSpawn;
+    public GameObject playerObject;
+    public List<Sprite> sprites;
+    float fireRate = 0.7f;
+
+    float fireTime;
+    float popTime;
+    float popDelay = 0f;
+    Vector3 targetUp;
+    Vector3 targetDown;
+    Vector3 velocity = Vector3.zero;
+    float moveSpeedUp = 0.04f;
+    bool outside = false;
+    //sprite change delay
+    float spriteDelay;
+    float spriteTime;
+    bool spriteChanged = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteDelay = fireRate / 2;
+        popTime = Time.time + popDelay;
+        targetUp = stopperBody.transform.position + new Vector3(0f, 0.7f, 0f);
+        targetDown = stopperBody.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //obrni proti playerju
+        if (playerObject.transform.position.x > stopperBody.transform.position.x)
+        {
+            stopperBody.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+        else
+        {
+            stopperBody.transform.rotation = new Quaternion(0, 180, 0, 0);
+        }
+        if (Time.time > popTime && !outside)
+        {
+            stopperBody.transform.position = Vector3.SmoothDamp(stopperBody.transform.position,
+                                                                targetUp,
+                                                                ref velocity,
+                                                                moveSpeedUp);
+            
+            
+        }
+        if (stopperBody.transform.position==targetUp && !outside) {
+            outside = true;
+            fireTime = Time.time + fireRate + globalDelay;
+        }
+        if (Time.time > fireTime && outside) {
+            spriteChanged = true;
+            spriteTime = Time.time + spriteDelay;
+            stopperBody.GetComponent<SpriteRenderer>().sprite = sprites[1];
+            float dir = 1;
+            for (int i = 0; i < 2; i++) {
+                GameObject projectile = Instantiate(stopperProjectile, projectileSpawn.transform.position, projectileSpawn.transform.rotation);
+                projectile.GetComponent<StopperProjectileController>().globalDirection = 1f*dir;
+                dir = dir * (-1);
+            }
+            fireTime = Time.time + fireRate + globalDelay;
+
+        }
+        if (Time.time > spriteTime && spriteChanged)
+        {
+            spriteChanged = false;
+            stopperBody.GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+    }
+}
---
+++ Assets/Scripts/Monsters/Stopper/StopperProjectileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopperProjectileController : MonoBehaviour
+{
+    public float globalDirection = -1;
+     float shootVelocityDirection = 2.5f;
+     float shootVelocityUp = 2.5f;
+    float revSpeed = 360f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(globalDirection * Vector2.right * shootVelocityDirection, ForceMode2D.Impulse);
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * shootVelocityUp, ForceMode2D.Impulse);
+        GetComponent<Rigidbody2D>().MoveRotation(GetComponent<Rigidbody2D>().rotation + revSpeed * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
