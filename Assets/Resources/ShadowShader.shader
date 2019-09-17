06ca60e3cacf7185c9c34b3726c83921e129e840
urby100 <urbanbozic1996@gmail.com>
2019-09-17 14:14:24 +0200

    Re-organized Files.

---
+++ Assets/Resources/ShadowShader.shader
@@ -0,0 +1,69 @@
+﻿Shader "Custom/ShadowShader"
+{
+	Properties{
+
+	   _DesiredColor("Desired Color ", Color) = (0,0,0,0.4)
+	   _MainTex("Base (RGB)", 2D) = "white" {}
+	   _ShadowIntensity("ShadowIntesity", Range(0, 3)) = 0.5
+	}
+
+		SubShader{
+		   Tags { "Queue" = "Transparent" }
+		   Cull Off
+
+		   Pass {
+			   Stencil {
+				   Ref 2
+				   Comp NotEqual
+				   Pass Replace
+			   }
+
+				Blend SrcAlpha OneMinusSrcAlpha
+
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				#include "UnityCG.cginc"
+
+				sampler2D _MainTex;
+				float4 _DesiredColor;
+				float _ShadowIntensity;
+
+				struct v2f {
+					half4 pos : POSITION;
+					half2 uv : TEXCOORD0;
+					fixed4 worldpos : WORLDPOS;
+				};
+
+				v2f vert(appdata_img v) {
+					v2f o;
+					o.worldpos = mul(unity_ObjectToWorld, fixed4(v.vertex.x, v.vertex.y, 0, 1));
+					o.pos = UnityObjectToClipPos(v.vertex);
+					half2 uv = MultiplyUV(UNITY_MATRIX_TEXTURE0, v.texcoord);
+					o.uv = uv;
+					return o;
+				}
+
+				half4 frag(v2f i) : COLOR {
+					half4 color = tex2D(_MainTex, i.uv);//dobimo barvo pixla texture
+					if (color.a == 0.0)//če je del texture neviden to pustimo
+						discard;
+					else
+						if (i.worldpos.y > 0)//če je pixel nad linijo, kjer se senca začne jo skrijemo 
+							
+							color.a = 0;
+						else
+							color = _DesiredColor;//nastavimo barvo pixla, ki smo jo nastavili v unity nastavitvah
+							//nastavimo intenzivnost sence glede na y
+							//y je 0 ali manjše
+							color.a = color.a *(_ShadowIntensity + i.worldpos.y);
+						
+					return color;
+				}
+				ENDCG
+		   }
+
+	}
+
+		Fallback off
+}
---
+++ Assets/Scripts/Effects/ShadowScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEngine.ParticleSystem;
+
+public class ShadowScript : MonoBehaviour
+{
+    
+    GameObject shadow;
+    SpriteRenderer sprRndShadow;
+    float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Boss") {
+            return;
+        }
+        shadow = new GameObject();
+        shadow.layer = LayerMask.NameToLayer("Shadows");
+        shadow.name = "ShadowSprite";
+        shadow.transform.parent = transform;
+        sprRndShadow = shadow.AddComponent<SpriteRenderer>();
+        Material m = Resources.Load<Material>("ShadowMaterial");
+        if (gameObject.name == "Trail" || gameObject.name == "SpitterProjectileTrail")
+        {
+            TrailRenderer tr= shadow.AddComponent<TrailRenderer>();
+            TrailRenderer parentTr = gameObject.GetComponent<TrailRenderer>();
+
+            tr.material = m;
+            tr.sortingLayerName = "Foreground";
+            tr.sortingOrder = 99;
+            tr.time = parentTr.time;
+            tr.minVertexDistance = parentTr.minVertexDistance;
+            tr.autodestruct = true;
+            tr.widthCurve = parentTr.widthCurve;
+            tr.widthMultiplier = parentTr.widthMultiplier;
+        }
+        sprRndShadow.material = m;
+        
+        sprRndShadow.color = new Color(0, 0, 0, 0.4f);
+        sprRndShadow.flipY = true;
+        sprRndShadow.sortingLayerName = "Foreground";
+        sprRndShadow.sortingOrder = 99;
+        sprRndShadow.transform.localScale = new Vector3(1, 1, 1);
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (SceneManager.GetActiveScene().name == "Boss")
+        {
+            return;
+        }
+
+        height = GetComponent<Renderer>().bounds.size.y / 2;
+        shadow.transform.position = new Vector3(gameObject.transform.position.x,
+            0.01f - height, 0);
+        shadow.transform.position += new Vector3(0,
+            (height- gameObject.transform.position.y),
+            0);
+        shadow.transform.localEulerAngles =new Vector3( gameObject.transform.localEulerAngles.x,0, -2*gameObject.transform.localEulerAngles.z);
+    }
+    void FixedUpdate()
+    {
+        if (SceneManager.GetActiveScene().name == "Boss")
+        {
+            return;
+        }
+        if (gameObject.GetComponent<SpriteRenderer>() != null)
+        {
+            sprRndShadow.sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        }
+        else {
+        }
+    }
+}
